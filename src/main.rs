47ac698fd91be008d34c0ec8d7be27f08464a0cb
Olexy <lemondead1@gmail.com>
2025-03-02 20:57:57 +0300

    Fixed window displaying as Unknown in Gnome

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::rc::Rc;
 use std::error::Error;
 use std::f64::consts::PI;
 use std::fmt::{Display, Formatter};
+use std::rc::Rc;
 use std::time::{Duration, Instant};
 
 use cairo::{Context, Operator, XCBConnection, XCBDrawable, XCBSurface, XCBVisualType};
-use glam::{DVec2, IVec2, UVec2};
+use glam::{DVec2, IVec2};
 use x::*;
-use xcb::{Connection, render, shape, VoidCookie, x, Xid};
-use xcb::x::Mapping::Keyboard;
+use xcb::{render, shape, x, Connection, VoidCookie, Xid};
 
-use crate::geom::{closest_point_below_line_on_circle};
+use crate::geom::closest_point_below_line_on_circle;
 
 mod geom;
 
@@ -30,7 +29,8 @@ xcb::atoms_struct! {
 }
 
 const RULER_HALF_WIDTH: f64 = 40.0;
-const TITLE: &str = "Ruler";
+const TITLE: &[u8] = b"Ruler";
+const CLASS: &[u8] = b"Ruler\0Ruler\0";
 const INITIAL_LENGTH: f64 = 400.0;
 const CONTROL_RADIUS: f64 = 20.0;
 const MIN_LENGTH: f64 = 200.0;
@@ -64,10 +64,6 @@ impl WindowGeometry {
     fn pos(&self) -> IVec2 {
         IVec2::new(self.x as i32, self.y as i32)
     }
-
-    fn size(&self) -> UVec2 {
-        UVec2::new(self.w as u32, self.h as u32)
-    }
 }
 
 #[derive(Copy, Clone)]
@@ -88,7 +84,13 @@ struct XCBObjects {
     visual_type: Visualtype,
 }
 
-fn check_versions(client_major: u32, client_minor: u32, server_major: u32, server_minor: u32, extension: &'static str) -> Result<(), Box<VersionMismatchError>> {
+fn check_versions(
+    client_major: u32,
+    client_minor: u32,
+    server_major: u32,
+    server_minor: u32,
+    extension: &'static str,
+) -> Result<(), Box<VersionMismatchError>> {
     if server_major != client_major || server_major != client_major {
         Err(Box::new(VersionMismatchError {
             client_major_version: client_major,
@@ -111,26 +113,53 @@ impl XCBObjects {
             client_minor_version: render::MINOR_VERSION,
         });
         let reply = conn.wait_for_reply(cookie)?;
-        check_versions(render::MAJOR_VERSION, render::MINOR_VERSION,
-                       reply.major_version(), reply.minor_version(), render::XNAME)?;
+        check_versions(
+            render::MAJOR_VERSION,
+            render::MINOR_VERSION,
+            reply.major_version(),
+            reply.minor_version(),
+            render::XNAME,
+        )?;
 
         let cookie = conn.send_request(&shape::QueryVersion {});
         let reply = conn.wait_for_reply(cookie)?;
-        check_versions(shape::MAJOR_VERSION, shape::MINOR_VERSION,
-                       reply.major_version() as u32, reply.minor_version() as u32, render::XNAME)?;
-
+        check_versions(
+            shape::MAJOR_VERSION,
+            shape::MINOR_VERSION,
+            reply.major_version() as u32,
+            reply.minor_version() as u32,
+            render::XNAME,
+        )?;
 
         let xcb = {
             let atoms = Atoms::intern_all(&conn)?;
             let screen = conn.get_setup().roots().nth(screen_num as usize).unwrap();
             let screen_buf = screen.to_owned();
             let colormap: Colormap = conn.generate_id();
-            let depth = screen.allowed_depths().find(|d| d.depth() == 32).unwrap().to_owned();
-            let visual_type = depth.visuals().iter().find(|v| v.class() == VisualClass::TrueColor).unwrap().clone();
+            let depth = screen
+                .allowed_depths()
+                .find(|d| d.depth() == 32)
+                .unwrap()
+                .to_owned();
+            let visual_type = depth
+                .visuals()
+                .iter()
+                .find(|v| v.class() == VisualClass::TrueColor)
+                .unwrap()
+                .clone();
             let window: Window = conn.generate_id();
             let gcontext = conn.generate_id();
 
-            XCBObjects { conn, atoms, screen: screen_buf, depth, visual_type, window, gcontext, colormap }
+            XCBObjects {
+                conn,
+                atoms,
+                screen: screen_buf,
+                depth,
+                visual_type,
+                window,
+                gcontext,
+                colormap,
+            }
         };
 
         let root = xcb.screen.root();
@@ -156,8 +185,15 @@ impl XCBObjects {
             value_list: &[
                 Cw::BorderPixel(0x00000000),
                 Cw::WinGravity(Gravity::NorthWest),
-                Cw::EventMask(EventMask::EXPOSURE | EventMask::KEY_PRESS | EventMask::BUTTON_PRESS | EventMask::BUTTON_RELEASE | EventMask::POINTER_MOTION | EventMask::STRUCTURE_NOTIFY),
-                Cw::Colormap(xcb.colormap)
+                Cw::EventMask(
+                    EventMask::EXPOSURE
+                        | EventMask::KEY_PRESS
+                        | EventMask::BUTTON_PRESS
+                        | EventMask::BUTTON_RELEASE
+                        | EventMask::POINTER_MOTION
+                        | EventMask::STRUCTURE_NOTIFY,
+                ),
+                Cw::Colormap(xcb.colormap),
             ],
         })?;
 
@@ -172,9 +208,17 @@ impl XCBObjects {
         xcb.conn.send_and_check_request(&ChangeProperty {
             mode: PropMode::Replace,
             window: xcb.window,
+            property: ATOM_WM_CLASS,
+            r#type: ATOM_STRING,
+            data: CLASS,
+        })?;
+
+        xcb.conn.send_and_check_request(&ChangeProperty {
+            mode: PropMode::Replace,
+            window: xcb.window,
             property: ATOM_WM_NAME,
             r#type: ATOM_STRING,
-            data: TITLE.as_bytes(),
+            data: TITLE,
         })?;
 
         xcb.conn.send_and_check_request(&ChangeProperty {
@@ -190,7 +234,10 @@ impl XCBObjects {
             window: xcb.window,
             property: xcb.atoms.net_wm_state,
             r#type: ATOM_ATOM,
-            data: &[xcb.atoms.net_wm_state_above, xcb.atoms.new_wm_state_skip_pager],
+            data: &[
+                xcb.atoms.net_wm_state_above,
+                xcb.atoms.new_wm_state_skip_pager,
+            ],
         })?;
 
         xcb.conn.send_and_check_request(&ChangeProperty {
@@ -204,10 +251,14 @@ impl XCBObjects {
         xcb.conn.send_and_check_request(&CreateGc {
             cid: xcb.gcontext,
             drawable: Drawable::Window(xcb.window),
-            value_list: &[Gc::Background(xcb.screen.black_pixel()), Gc::GraphicsExposures(false)],
+            value_list: &[
+                Gc::Background(xcb.screen.black_pixel()),
+                Gc::GraphicsExposures(false),
+            ],
         })?;
 
-        xcb.conn.send_and_check_request(&MapWindow { window: xcb.window })?;
+        xcb.conn
+            .send_and_check_request(&MapWindow { window: xcb.window })?;
 
         Ok(xcb)
     }
@@ -245,7 +296,12 @@ impl XCBObjects {
             drawable: Drawable::Window(window),
         });
         let reply = self.conn.wait_for_reply(cookie)?;
-        Ok(WindowGeometry { x: reply.x(), y: reply.y(), w: reply.width(), h: reply.height() })
+        Ok(WindowGeometry {
+            x: reply.x(),
+            y: reply.y(),
+            w: reply.width(),
+            h: reply.height(),
+        })
     }
 }
 
@@ -257,14 +313,27 @@ struct Render {
 impl Render {
     fn setup(xcb: &XCBObjects, height: u16, width: u16) -> Result<Render, Box<dyn Error>> {
         let surface = unsafe {
-            let cairo_conn = XCBConnection::from_raw_none(xcb.conn.get_raw_conn() as *mut cairo::ffi::xcb_connection_t);
-            let visual_type = XCBVisualType::from_raw_none(&xcb.visual_type as *const Visualtype as *mut cairo::ffi::xcb_visualtype_t);
+            let cairo_conn = XCBConnection::from_raw_none(
+                xcb.conn.get_raw_conn() as *mut cairo::ffi::xcb_connection_t
+            );
+            let visual_type = XCBVisualType::from_raw_none(
+                &xcb.visual_type as *const Visualtype as *mut cairo::ffi::xcb_visualtype_t,
+            );
             let drawable = XCBDrawable(xcb.window.resource_id());
-            XCBSurface::create(&cairo_conn, &drawable, &visual_type, width as i32, height as i32)?
+            XCBSurface::create(
+                &cairo_conn,
+                &drawable,
+                &visual_type,
+                width as i32,
+                height as i32,
+            )?
         };
         xcb.conn.flush()?;
         let cairo = Context::new(&surface)?;
-        Ok(Render { ctx: cairo, surface })
+        Ok(Render {
+            ctx: cairo,
+            surface,
+        })
     }
 
     fn resize(&self, width: i32, height: i32) -> Result<(), Box<dyn Error>> {
@@ -274,7 +343,10 @@ impl Render {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let xcb = Rc::new(XCBObjects::setup((INITIAL_LENGTH + RULER_HALF_WIDTH * 2.0) as u16, (RULER_HALF_WIDTH * 2.0) as u16)?);
+    let xcb = Rc::new(XCBObjects::setup(
+        (INITIAL_LENGTH + RULER_HALF_WIDTH * 2.0) as u16,
+        (RULER_HALF_WIDTH * 2.0) as u16,
+    )?);
 
     let root_geom = xcb.get_window_geometry(xcb.screen.root())?;
 
@@ -282,7 +354,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         let from_x = (root_geom.w as f64 - INITIAL_LENGTH) / 2.0 + RULER_HALF_WIDTH;
         let from_y = root_geom.h as f64 / 2.0 + RULER_HALF_WIDTH;
 
-        (DVec2::new(from_x, from_y), DVec2::new(from_x + INITIAL_LENGTH, from_y))
+        (
+            DVec2::new(from_x, from_y),
+            DVec2::new(from_x + INITIAL_LENGTH, from_y),
+        )
     };
 
     let render = {
@@ -319,33 +394,48 @@ fn main() -> Result<(), Box<dyn Error>> {
                     }
                 }
             }
-            xcb::Event::X(Event::MotionNotify(ev)) => {
-                match dragging {
-                    Dragging::From => {
-                        let screen_size = DVec2::new(root_geom.w as f64, root_geom.h as f64);
-                        let fix_distance = ev.state().intersects(KeyButMask::CONTROL);
-                        let fix_angle = ev.state().intersects(KeyButMask::SHIFT);
-                        handle_drag(&mut from, to, DVec2::new(ev.root_x() as f64, ev.root_y() as f64), screen_size, fix_distance, fix_angle);
-                        if let Some(_) = update(&xcb, &render, from, to, &mut last_update, false) {
-                            xcb.conn.flush()?;
-                        }
+            xcb::Event::X(Event::MotionNotify(ev)) => match dragging {
+                Dragging::From => {
+                    let screen_size = DVec2::new(root_geom.w as f64, root_geom.h as f64);
+                    let fix_distance = ev.state().intersects(KeyButMask::CONTROL);
+                    let fix_angle = ev.state().intersects(KeyButMask::SHIFT);
+                    handle_drag(
+                        &mut from,
+                        to,
+                        DVec2::new(ev.root_x() as f64, ev.root_y() as f64),
+                        screen_size,
+                        fix_distance,
+                        fix_angle,
+                    );
+                    if let Some(_) = update(&xcb, &render, from, to, &mut last_update, false) {
+                        xcb.conn.flush()?;
                     }
-                    Dragging::To => {
-                        let screen_size = DVec2::new(root_geom.w as f64, root_geom.h as f64);
-                        let fix_distance = ev.state().intersects(KeyButMask::CONTROL);
-                        let fix_angle = ev.state().intersects(KeyButMask::SHIFT);
-                        handle_drag(&mut to, from, DVec2::new(ev.root_x() as f64, ev.root_y() as f64), screen_size, fix_distance, fix_angle);
-                        if let Some(_) = update(&xcb, &render, from, to, &mut last_update, false) {
-                            xcb.conn.flush()?;
-                        }
-                    }
-                    Dragging::None => {}
                 }
-            }
+                Dragging::To => {
+                    let screen_size = DVec2::new(root_geom.w as f64, root_geom.h as f64);
+                    let fix_distance = ev.state().intersects(KeyButMask::CONTROL);
+                    let fix_angle = ev.state().intersects(KeyButMask::SHIFT);
+                    handle_drag(
+                        &mut to,
+                        from,
+                        DVec2::new(ev.root_x() as f64, ev.root_y() as f64),
+                        screen_size,
+                        fix_distance,
+                        fix_angle,
+                    );
+                    if let Some(_) = update(&xcb, &render, from, to, &mut last_update, false) {
+                        xcb.conn.flush()?;
+                    }
+                }
+                Dragging::None => {}
+            },
             xcb::Event::X(Event::ButtonRelease(ev)) => {
                 if ev.detail() == 1 {
                     dragging = Dragging::None;
-                    let pos = update(&xcb, &render, from, to, &mut last_update, true).unwrap().pos().as_dvec2();
+                    let pos = update(&xcb, &render, from, to, &mut last_update, true)
+                        .unwrap()
+                        .pos()
+                        .as_dvec2();
                     xcb.set_window_shape_from_points(from - pos, to - pos);
                     xcb.conn.flush()?;
                 }
@@ -367,18 +457,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
-fn update(xcb: &XCBObjects, render: &Render, from: DVec2, to: DVec2, last_update: &mut Instant, force: bool) -> Option<(WindowGeometry)> {
+fn update(
+    xcb: &XCBObjects,
+    render: &Render,
+    from: DVec2,
+    to: DVec2,
+    last_update: &mut Instant,
+    force: bool,
+) -> Option<WindowGeometry> {
     let now = Instant::now();
     if force || now - *last_update > Duration::from_millis(16) {
         let geometry = compute_window_geometry(from, to);
-        render.resize(geometry.w as i32, geometry.h as i32);
+        render.resize(geometry.w as i32, geometry.h as i32).ok()?;
         xcb.conn.send_request(&ConfigureWindow {
             window: xcb.window,
             value_list: &[
                 ConfigWindow::X(geometry.x as i32),
                 ConfigWindow::Y(geometry.y as i32),
                 ConfigWindow::Width(geometry.w as u32),
-                ConfigWindow::Height(geometry.h as u32)
+                ConfigWindow::Height(geometry.h as u32),
             ],
         });
         *last_update = now;
@@ -408,18 +505,31 @@ fn compute_window_geometry(from: DVec2, to: DVec2) -> WindowGeometry {
     }
 }
 
-fn handle_drag(dragging: &mut DVec2, other: DVec2, cursor: DVec2, screen_size: DVec2, fix_distance: bool, fix_angle: bool) {
+fn handle_drag(
+    dragging: &mut DVec2,
+    other: DVec2,
+    cursor: DVec2,
+    screen_size: DVec2,
+    fix_distance: bool,
+    fix_angle: bool,
+) {
     let mut new_vec = cursor;
 
     if fix_distance {
-        let new_diff_normalized = (new_vec - other).try_normalize().unwrap_or(DVec2::new(1.0, 0.0));
+        let new_diff_normalized = (new_vec - other)
+            .try_normalize()
+            .unwrap_or(DVec2::new(1.0, 0.0));
         let old_distance = dragging.distance(other);
         new_vec = other + new_diff_normalized * old_distance;
 
-        new_vec = closest_point_below_line_on_circle(other, old_distance, DVec2::ZERO, DVec2::X, new_vec);
-        new_vec = closest_point_below_line_on_circle(other, old_distance, screen_size, DVec2::X, new_vec);
-        new_vec = closest_point_below_line_on_circle(other, old_distance, DVec2::ZERO, DVec2::Y, new_vec);
-        new_vec = closest_point_below_line_on_circle(other, old_distance, screen_size, DVec2::Y, new_vec);
+        new_vec =
+            closest_point_below_line_on_circle(other, old_distance, DVec2::ZERO, DVec2::X, new_vec);
+        new_vec =
+            closest_point_below_line_on_circle(other, old_distance, screen_size, DVec2::X, new_vec);
+        new_vec =
+            closest_point_below_line_on_circle(other, old_distance, DVec2::ZERO, DVec2::Y, new_vec);
+        new_vec =
+            closest_point_below_line_on_circle(other, old_distance, screen_size, DVec2::Y, new_vec);
     }
 
     if fix_angle {
@@ -482,11 +592,12 @@ fn draw(ctx: &Context, from: DVec2, to: DVec2) -> Result<(), Box<dyn Error>> {
     ctx.set_font_size(14.0);
 
     for i in (0..length_pixels).step_by(5) {
-        let inner_width = RULER_HALF_WIDTH - match i % 50 {
-            0 => 17.0,
-            25 => 12.0,
-            _ => 7.0
-        };
+        let inner_width = RULER_HALF_WIDTH
+            - match i % 50 {
+                0 => 17.0,
+                25 => 12.0,
+                _ => 7.0,
+            };
 
         ctx.line_to(i as f64, -inner_width);
         ctx.line_to(i as f64, -RULER_HALF_WIDTH);
@@ -508,7 +619,12 @@ fn draw(ctx: &Context, from: DVec2, to: DVec2) -> Result<(), Box<dyn Error>> {
     ctx.arc(0.0, 0.0, 16.0, 0.0, -angle);
     ctx.stroke()?;
 
-    let display_angle = if angle > 0.0 { PI * 2.0 - angle } else { angle.abs() } * 180.0 / PI;
+    let display_angle = if angle > 0.0 {
+        PI * 2.0 - angle
+    } else {
+        angle.abs()
+    } * 180.0
+        / PI;
     let angle_string = format!("{:.2}°", display_angle);
     let extents = ctx.text_extents(&angle_string)?;
     ctx.translate(35.0, extents.height());
